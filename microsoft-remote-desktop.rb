1562024c46646b5d68cd092d666e3cb5703dde4f
moogle19 <mail@kseidel.org>
2016-05-27 12:20:53 +0200

    Add checksum

---
+++ microsoft-remote-desktop.rb
@@ -0,0 +1,15 @@
+cask 'microsoft-remote-desktop' do
+  version :latest
+  sha256 '9f81dc05b3e0eecf27dace4d310e903b94743b0fc2bf5b93ac12f55958bb6673'
+
+  url 'https://dl.dropboxusercontent.com/u/15801038/mrd.pkg'
+  name 'Microsoft Office 2016'
+  homepage 'https://www.microsoft.com/mac'
+  license :commercial
+
+  pkg 'mrd.pkg'
+
+  uninstall pkgutil:   [
+                         'com.microsoft.rdc.*',
+                       ]
+end
