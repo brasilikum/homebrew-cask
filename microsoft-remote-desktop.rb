4426a13d88be1983c930e437cffa0c6fd87ff103
moogle19 <mail@kseidel.org>
2016-05-27 12:27:37 +0200

    Fix name

--- microsoft-remote-desktop.rb
+++ microsoft-remote-desktop.rb
@@ -3,7 +3,7 @@ cask 'microsoft-remote-desktop' do
   sha256 '9f81dc05b3e0eecf27dace4d310e903b94743b0fc2bf5b93ac12f55958bb6673'
 
   url 'https://dl.dropboxusercontent.com/u/15801038/mrd.pkg'
-  name 'Microsoft Office 2016'
+  name 'Microsoft Remote Desktop'
   homepage 'https://www.microsoft.com/mac'
   license :commercial
 
